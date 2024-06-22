f640692297c2a62d46a5426204401fd299896587
anand.kumar <anandgupta372@gmail.com>
2024-06-23 01:09:12 +0530

    jenkins first compilation

---
+++ JenkinsTest.java
@@ -0,0 +1,13 @@
+package com.anand;
+
+public class JenkinsTest {
+
+	
+	public static void main(String[] args) {
+	  
+		  System.out.println("Hellow World Java to jenkins CI/CD Pipeline Example");
+		
+
+	}
+
+}
